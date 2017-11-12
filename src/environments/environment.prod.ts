32901562368e80a3da02da4b38f1b0eb54426936
Kavita <kavya.yadav27@gmail.com>
2017-11-12 12:55:59 +0530

    misc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyADCgc5AZRDMPE6G7rMmVUwm_gFmFGYz_o",
+    authDomain: "stories-around.firebaseapp.com",
+    databaseURL: "https://stories-around.firebaseio.com",
+    projectId: "stories-around",
+    storageBucket: "stories-around.appspot.com",
+    messagingSenderId: "932616060519"
+  }
 };
